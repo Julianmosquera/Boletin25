4c61e7e65279b61ecaab5788a913a12e538fa36c
julianMGCODE <julianmosquerag@gmail.com>
2016-04-08 12:06:45 +0200

    FIN del B

---
+++ Boletin25_2/src/boletin25_2/Boletin25_2.java
@@ -0,0 +1,16 @@
+
+package boletin25_2;
+
+/**
+ *
+ * @author julian
+ */
+public class Boletin25_2 {
+
+   
+    public static void main(String[] args) {
+      Ventana v= new Ventana();
+      v.setVisible(true);
+    }
+    
+}
