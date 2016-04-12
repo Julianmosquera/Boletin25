ecb724bef617a8f22123cd1d560cbf810f094c82
julianMGCODE <julianmosquerag@gmail.com>
2016-04-12 08:56:11 +0200

    Fin 25_3

---
+++ Boletin25_3/src/boletin25_3/Boletin25_3.java
@@ -0,0 +1,16 @@
+
+package boletin25_3;
+
+/**
+ *
+ * @author julian
+ */
+public class Boletin25_3 {
+
+    
+    public static void main(String[] args) {
+        Ventana v=new Ventana();
+        v.setVisible(true);
+    }
+    
+}
