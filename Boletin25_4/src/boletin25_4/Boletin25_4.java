a509ebed00b60f45f89b944cf4b4deeb24477dd1
julianMGCODE <julianmosquerag@gmail.com>
2016-04-12 09:14:05 +0200

    Fin 25_4

---
+++ Boletin25_4/src/boletin25_4/Boletin25_4.java
@@ -0,0 +1,21 @@
+
+package boletin25_4;
+
+/**
+ *
+ * @author julian
+ */
+public class Boletin25_4 {
+
+  
+    public static void main(String[] args) {
+       Ventana1 ven1= new Ventana1();
+       ven1.setVisible(true);
+       
+       
+       
+       Ventana2 ven2= new Ventana2();
+       ven2.setVisible(true);
+    }
+    
+}
